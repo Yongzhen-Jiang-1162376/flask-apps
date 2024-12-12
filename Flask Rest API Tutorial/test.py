cbb18ace28896ca89516ae0a8d72ef5b3b1d7943
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2024-12-13 11:06:00 +1300

    add resource crud functions

---
+++ Flask Rest API Tutorial/test.py
@@ -0,0 +1,26 @@
+import requests
+
+
+BASE = 'http://localhost:5000/'
+
+# headers = {'accept': 'application/json'}
+
+data = [
+    {'likes': 78, 'name': 'Joe', 'views': 10000},
+    {'likes': 1000, 'name': 'How to make REST API', 'views': 80000},
+    {'likes': 35, 'name': 'Tim', 'views': 2000}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + 'video/' + str(i), data[i])
+    print(response.json())
+
+input()
+
+response = requests.delete(BASE + 'video/0')
+print(response)
+
+input()
+
+response = requests.get(BASE + 'video/1')
+print(response.json())
