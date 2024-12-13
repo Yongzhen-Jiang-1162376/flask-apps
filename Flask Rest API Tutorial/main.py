d68093de18e8a813a872bcd2ce177f3353d9fb73
Yongzhen Jiang <yongzhen.jiang@gmail.com>
2024-12-13 14:59:18 +1300

    update patch function

--- Flask Rest API Tutorial/main.py
+++ Flask Rest API Tutorial/main.py
@@ -17,31 +17,21 @@ class VideoModel(db.Model):
         return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
 
 
-# db.create_all()
+# with app.app_context():
+#     db.create_all()
+
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument('name', type=str, help='Name of the video is required', location='form', required=True)
 video_put_args.add_argument('views', type=int, help='Views of the video is required', location = 'form', required=True)
 video_put_args.add_argument('likes', type=int, help='Likes of the video is required', location='form', required=True)
 
-# names = {
-#     'tim': {'age': 19, 'gender': 'male'},
-#     'bill': {'age': 70, 'gender': 'male'}
-# }
 
-# class HelloWorld(Resource):
-#     def get(self, name):
-#         return names[name]
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument('name', type=str, help='Name of the video is required', location='form')
+video_update_args.add_argument('views', type=int, help='Views of the video is required', location='form')
+video_update_args.add_argument('likes', type=int, help='Likes of the video is required', location='form')
 
-# videos = {}
-
-# def abort_if_video_id_doesnt_exist(video_id):
-#     if video_id not in videos:
-#         abort(404, message="Video id is not valid...")
-
-# def abort_if_video_exists(video_id):
-#     if video_id in videos:
-#         abort(409, message="Video already exists with that id...")
 
 resource_fields = {
     'id': fields.Integer,
@@ -55,6 +45,10 @@ class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
+        
+        if not result:
+            abort(404, message="Could not find video with that id")
+
         return result
 
     @marshal_with(resource_fields)
@@ -75,6 +69,26 @@ class Video(Resource):
         db.session.commit()
 
         return video, 201
+    
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+        
+        if args['name']:
+            result.name = args['name']
+        if args["views"]:
+            result.views = args['views']
+        if args["likes"]:
+            result.likes = args['likes']
+
+        # db.session.add(result)
+        db.session.commit()
+
+        return result
 
     def delete(self, video_id):
         pass
--- Flask Rest API Tutorial/test.py
+++ Flask Rest API Tutorial/test.py
@@ -5,17 +5,18 @@ BASE = 'http://localhost:5000/'
 
 # headers = {'accept': 'application/json'}
 
-data = [
-    {'likes': 78, 'name': 'Joe', 'views': 10000},
-    {'likes': 1000, 'name': 'How to make REST API', 'views': 80000},
-    {'likes': 35, 'name': 'Tim', 'views': 2000}
-]
+# data = [
+#     {'likes': 78, 'name': 'Joe', 'views': 10000},
+#     {'likes': 1000, 'name': 'How to make REST API', 'views': 80000},
+#     {'likes': 35, 'name': 'Tim', 'views': 2000}
+# ]
 
-for i in range(len(data)):
-    response = requests.put(BASE + 'video/' + str(i), data[i])
-    print(response.json())
+# for i in range(len(data)):
+#     response = requests.put(BASE + 'video/' + str(i), data[i])
+#     print(response.json())
 
-input()
+response = requests.get(BASE + 'video/0')
+print(response.json())
 
-response = requests.get(BASE + 'video/1')
+response = requests.patch(BASE + 'video/2', {"views": 203, "likes": 5})
 print(response.json())
